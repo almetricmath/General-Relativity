f5945af7adf9edaf260a433c4a2691ec4ebd137b
Al <al@matricmath.com>
2022-10-04 22:47:55 -0700

    added full tensor evaluation of transformed submatrices

--- TensorTransforms/TensorTransforms.py
+++ TensorTransforms/TensorTransforms.py
@@ -238,18 +238,19 @@ def computeFourthOrderTensorOuterProduct(_T, _Basis_1, _Basis_2, _Basis_3, _Basi
 
     b_4 = np.array(b_4)
     
-    ret=np.array([[[[[[0.0]*_n]*_n]*_n]*_n]*_n]*_n)
+    #ret=np.array([[[[[[0.0]*_n]*_n]*_n]*_n]*_n]*_n)
+    result = 0
 
     for i in range(_n):
         for j in range(_n):
-            result = 0
+            #result = 0
             for k in range(_n):
                 for l in range(_n):
                     tmp = _T[i, j, k, l]
                     result += tmp*np.einsum('i,j,k,l',b_1[k], b_2[l], b_3[i],b_4[j])
-            ret[i][j] = result
+            #ret[i][j] = result
         
-    return ret
+    return result
 
 
 def computeFourthOrderMatrixOuterProduct(_T, _Basis_1, _Basis_2, _Basis_3, _Basis_4, _n):
@@ -312,17 +313,17 @@ def convertMatrixToLatex(_result, _n):
     ret += '}'
     return ret
         
-def createLatexBlockMatrix(_matrixLst, _weights, _n):
+
+def convertResultToLatex(_result, _weights, _n):
     
-    
-    ret = '\\bmatrix{'
+    ret = '\\bmatrix{' 
     
     for i in range(_n):
         for j in range(_n):
-            index = i*_n + j
+            tmp = _result[i][j]
             if type(_weights) == np.ndarray:
-                ret += str('(' + "{:.8f}".format(_weights[i][j])) + ')'
-            ret += convertMatrixToLatex(_matrixLst[index], _n)
+                ret +=  str('(' + "{:.8f}".format(_weights[i][j])) + ')'
+            ret += convertMatrixToLatex(tmp, _n)
             if j != _n - 1:
                 ret += '&'
         if i != _n - 1:
@@ -332,27 +333,7 @@ def createLatexBlockMatrix(_matrixLst, _weights, _n):
     
     return ret
 
-           
-def convertResultsToLatex(_result, _weights, _n):
-    
-    ret = []
-    for i in range(_n):
-        for j in range(_n):
-            matrixLst = []
-            for k in range(_n):
-                for l in range(_n):
-                    matrixLst.append(_result[i][j][k][l])
-                
-            if type(_weights) == np.ndarray:
-                index = i*_n + j
-                weights = _weights[index]
-            else:
-                weights = 0
-            ret.append(createLatexBlockMatrix(matrixLst, weights, _n))
-    
-    return ret
-           
-
+        
 # compute 2nd order tensor in polar coordinates 
 # using both outer product and inner product
 
@@ -480,24 +461,288 @@ TW = [[ np.array([[1,2],[3,4]]), np.array([[5,6],[7,8]])], [np.array([[9,10],[11
 TW = np.array(TW)
 
 # compute weight matrix
- 
+
+
 result11 = computeFourthOrderTensorOuterProduct(TW, E, E, E, E, 2)
+l_results = convertResultToLatex(result11, 0, 2)
+print('Summed Results\n')
+print(l_results)
+print('\n')
 
  
 result12, coords, bases = computeFourthOrderMatrixOuterProduct(TW,E, E, E, E, 2)
-l_results = convertResultsToLatex(result12, 0, 2)
-l_weights = convertResultsToLatex(bases, coords, 2)
 
-for i in range(len(l_results)):
-    print('weights ' + str(i))
-    print('\n')
-    print(l_weights[i])
-    print('\n')
-    print('results ' + str(i))
-    print('\n')
-    print(l_results[i])
-    print('\n')
-    
+for i in range(2):
+    for j in range(2):
+        print('weighted sub-matrix',str(i),str(j),'\n')
+        index = i*2 + j
+        l_result = convertResultToLatex(bases[i][j], coords[index], 2)
+        print(l_result, '\n')
+        print('result sub-matrix', str(i), str(j),'\n')
+        l_result = convertResultToLatex(result12[i][j],0, 2)
+        print(l_result, '\n')
+        
+        
+# compute T_ij using  ET(TW)E       
+# T_11     
+   
+l_result = convertMatrixToLatex(E, 2)
+print('E Matrix')
+print(l_result)
+print('\n')
+
+l_result = convertMatrixToLatex(np.transpose(E), 2)
+print('ET Matrix')
+print(l_result)
+print('\n')
+
+# compute T_11 matrix
+
+# Test ET(Tijkl)E = ET(TW)E
+
+T11 = TW[0][0]
+tmp = np.dot(T11, E)
+T_11 = np.dot(np.transpose(E),tmp)
+l_result = convertMatrixToLatex(T_11, 2)
+print(' T_11 components - ET(T11)E\n')
+print(l_result)
+print('\n')
+
+# compute T_12 matrix
+
+T12 = TW[0][1]
+tmp = np.dot(T12, E)
+T_12 = np.dot(np.transpose(E),tmp)
+l_result = convertMatrixToLatex(T_12, 2)
+print(' T_12 components - ET(T12)E\n')
+print(l_result)
+print('\n')
+
+# compute T_21 matrix
+
+T21 = TW[1][0]
+tmp = np.dot(T21, E)
+T_21 = np.dot(np.transpose(E),tmp)
+l_result = convertMatrixToLatex(T_21, 2)
+print(' T_21 components - ET(T21)E\n')
+print(l_result)
+print('\n')
+
+# compute T_22 matrix
+
+T22 = TW[1][1]
+tmp = np.dot(T22, E)
+T_22 = np.dot(np.transpose(E),tmp)
+l_result = convertMatrixToLatex(T_22, 2)
+print(' T_22 components - ET(T22)E\n')
+print(l_result)
+print('\n')
+
+
+# add submatrices to get the final result
+summed_matrix = allocateFourthOrderElement(2)
+for i in range(2):
+    for j in range(2):
+        summed_matrix += result12[i][j]
+
+l_result = convertResultToLatex(summed_matrix, 0, 2)
+print('Summed Results\n')
+print(l_result)
+print('\n')
+        
+
+
+# 11 component
+
+T__11 = np.array([[coords[0][0][0],coords[1][0][0]],[coords[2][0][0],coords[3][0][0]]])
+l_result = convertMatrixToLatex(T__11, 2)
+print('T__11 matrix \n')
+print(l_result)
+print('\n')
+
+
+
+tmp = np.dot(T__11, E)
+Tsum_11 = np.dot(np.transpose(E),tmp)
+l_result = convertMatrixToLatex(Tsum_11, 2)
+print('Summed Results 11 components \n')
+print(l_result)
+print('\n')
+
+# transform T_kl back to T_ijkl using T_ijkl = W.T_kl.WT
+
+l_result = convertMatrixToLatex(W, 2)
+print('W matrix \n')
+print(l_result)
+print('\n')
+
+l_result = convertMatrixToLatex(np.transpose(W), 2)
+print('WT matrix \n')
+print(l_result)
+print('\n')
+
+
+tmp = np.dot(T_11, np.transpose(W))
+testT_ij11 = np.dot(W, tmp)
+l_result = convertMatrixToLatex(testT_ij11,2)
+print('testT_ij11 matrix \n')
+print(l_result)
+print('\n')
+
+# transform 4th order contravariant tensor
+# polar based transform
+
+r = 2
+theta = np.pi/3
+E = polarVectorBasis(r, theta)
+W = polarOneFormBasis(r, theta)
+r1 = r**2
+theta1 = theta**2
+A = polarSqrtAMatrix(r1, theta1)
+B = polarSqrtBMatrix(r1,theta1)
+E1 = np.dot(A,E)
+
+l_result = convertMatrixToLatex(E1,2)
+print('E1 matrix \n')
+print(l_result)
+print('\n')
+
+l_result = convertMatrixToLatex(np.transpose(E1),2)
+print('E1T matrix \n')
+print(l_result)
+print('\n')
+
+
+W1 = np.dot(np.transpose(B),W)
+
+
+#compute [T]i1,j1
+print('4th order coordinate change\n')
+
+
+
+l_result = convertMatrixToLatex(np.transpose(B),2)
+print('BT matrix \n')
+print(l_result)
+print('\n')
+
+l_result = convertMatrixToLatex(B,2)
+print('B matrix \n')
+print(l_result)
+print('\n')
+
+T__11 = np.array([[coords[0][0][0],coords[1][0][0]],[coords[2][0][0],coords[3][0][0]]])
+l_result = convertMatrixToLatex(T__11, 2)
+print('T__11 matrix \n')
+print(l_result)
+print('\n')
+
+# compute [T]_ij in the primed coordinate system
+# 11 component
+
+tmp = np.dot(T__11, B)
+Tprime__11 = np.dot(np.transpose(B),tmp)
+l_result = convertMatrixToLatex(Tprime__11, 2)
+print('Tprime__11 matrix\n')
+print(l_result)
+print('\n')
+
+# 12 component
+
+T__12 = np.array([[coords[0][0][1],coords[1][0][1]],[coords[2][0][1],coords[3][0][1]]])
+l_result = convertMatrixToLatex(T__12, 2)
+print('T__12 matrix \n')
+print(l_result)
+print('\n')
+
+tmp = np.dot(T__12, B)
+Tprime__12 = np.dot(np.transpose(B),tmp)
+l_result = convertMatrixToLatex(Tprime__12, 2)
+print('Tprime__12 matrix\n')
+print(l_result)
+print('\n')
+
+# 21 component
+
+T__21 = np.array([[coords[0][1][0],coords[1][1][0]],[coords[2][1][0],coords[3][1][0]]])
+l_result = convertMatrixToLatex(T__21, 2)
+print('T__21 matrix \n')
+print(l_result)
+print('\n')
+
+tmp = np.dot(T__21, B)
+Tprime__21 = np.dot(np.transpose(B),tmp)
+l_result = convertMatrixToLatex(Tprime__21, 2)
+print('Tprime__21 matrix\n')
+print(l_result)
+print('\n')
+
+# 22 component
+
+T__22 = np.array([[coords[0][1][1],coords[1][1][1]],[coords[2][1][1],coords[3][1][1]]])
+l_result = convertMatrixToLatex(T__22, 2)
+print('T__22 matrix \n')
+print(l_result)
+print('\n')
+
+tmp = np.dot(T__22, B)
+Tprime__22 = np.dot(np.transpose(B),tmp)
+l_result = convertMatrixToLatex(Tprime__22, 2)
+print('Tprime__22 matrix\n')
+print(l_result)
+print('\n')
+
+# compute the complete tensor for each submatrix
+
+# 11 component
+
+tmp = np.dot(Tprime__11, E1)
+Tprime__11Sum = np.dot(np.transpose(E1),tmp)
+l_result = convertMatrixToLatex(Tprime__11Sum, 2)
+print('Tprime__11Sum matrix\n')
+print(l_result)
+print('\n')
+
+# 12 component
+
+tmp = np.dot(Tprime__12, E1)
+Tprime__12Sum = np.dot(np.transpose(E1),tmp)
+l_result = convertMatrixToLatex(Tprime__12Sum, 2)
+print('Tprime__12Sum matrix\n')
+print(l_result)
+print('\n')
+
+# 21 component
+
+tmp = np.dot(Tprime__21, E1)
+Tprime__21Sum = np.dot(np.transpose(E1),tmp)
+l_result = convertMatrixToLatex(Tprime__21Sum, 2)
+print('Tprime__21Sum matrix\n')
+print(l_result)
+print('\n')
+
+# 22 component
+
+tmp = np.dot(Tprime__22, E1)
+Tprime__22Sum = np.dot(np.transpose(E1),tmp)
+l_result = convertMatrixToLatex(Tprime__22Sum, 2)
+print('Tprime__22Sum matrix\n')
+print(l_result)
+print('\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
