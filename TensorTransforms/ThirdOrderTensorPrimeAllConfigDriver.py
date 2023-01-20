e4c8de67f4c341c8e5e965a1b1bafb9d0384565e
Al <al@matricmath.com>
2023-01-19 18:13:40 -0800

    updated driver to also run configurations in reverse

--- TensorTransforms/ThirdOrderTensorPrimeAllConfigDriver.py
+++ TensorTransforms/ThirdOrderTensorPrimeAllConfigDriver.py
@@ -92,7 +92,6 @@ posLst = [ [t.pos.up, t.pos.up, t.pos.down], [t.pos.up, t.pos.down, t.pos.up],  
 
 inPosLst = [t.pos.up, t.pos .up, t.pos.up]
 
-
 for item in posLst:
     T_config = 0
     T_config = t3.changeConfig(T_init, inPosLst, item, G, Ginv, 2, verbose)
@@ -121,9 +120,50 @@ for item in posLst:
     print('Difference between the inner product computations in different configurations in primed coordinates and the outer product of the original configuration in unprimed coordinates\n') 
     print(diff_result, '\n\n')
  
+
+posLst = [ [t.pos.up, t.pos.up, t.pos.down], [t.pos.up, t.pos.down, t.pos.up],  [t.pos.up, t.pos.down, t.pos.down], [t.pos.down, t.pos.up, t.pos.up], [t.pos.down, t.pos.up, t.pos.down], [t.pos.down, t.pos.down, t.pos.up], [t.pos.down, t.pos.down, t.pos.down]] 
+posLst.reverse()
+
+del posLst[0]
+posLst[-1] = [t.pos.up, t.pos.up, t.pos.up]
+
+inPosLst = [t.pos.down, t.pos.down, t.pos.down]
+ 
+T_init = np.array([[[  1.,   8.], [  3.,  16.]], [[ 20.,  96.], [ 28., 128.]]])
+
+print('Running configurations in Reverse\n')
+print('Initial Configuration = ', inPosLst, '\n')
+print('T = ', T_init, '\n')
+
+for item in posLst:
+    T_config = 0
+    T_config = t3.changeConfig(T_init, inPosLst, item, G, Ginv, 2, verbose)
+    T1_n, T1_ijk = t3.transformTensor(T_config, item, 2, verbose) # transform from unprimed to prime coordinates
+
+    print('configuration = ', item, '\n')
+    print('T̅ = ', T1_ijk, '\n')
+    unprimed = False # computing tensor in primed coordinate system
+    result6 = t3.computeTensorOuterProduct(T1_ijk, item, unprimed, 2, verbose)
+    print('3rd order tensor by outer product\n')
+    l_result = t3.convertToLatex(result6, 2)
+    print(l_result)
+    print('\n')
     
+    diff_result = t3.convertToLatex(result6 - result, 2)
+
+    print('Difference between the outer product tensor in primed coordinates in different configurations and the outer product of the original configuration in unprimed coordinates\n') 
+    print(diff_result, '\n\n')
     
-    
+    print('3rd order tensor by inner product\n')
+    result7 = t3.computeTensorInnerProduct(T1_n, item, unprimed, 2, verbose)
+    l_result = t3.convertToLatex(result7, 2)
+    print(l_result, '\n')
+
+    diff_result = t3.convertToLatex(result7 - result, 2)
+    print('Difference between the inner product computations in different configurations in primed coordinates and the outer product of the original configuration in unprimed coordinates\n') 
+    print(diff_result, '\n\n')
+ 
+
     
 
 
